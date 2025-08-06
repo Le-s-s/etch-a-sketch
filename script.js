97b745360dc859ba56e125af1363e0a8b33a06f9
Le-s-s <lexton.d.lewis1@gmail.com>
2025-08-06 11:57:24 -0600

    add increment opacity and random color

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let page = document.querySelector('.container')
-    let button = document.querySelector('.button')
+    let page = document.querySelector('.container');
+    let button = document.querySelector('.button');
 
     button.addEventListener("click", (e) => {
         page.innerHTML = "";
         let drawSizeX = document.querySelector(".x").value
         let drawSizeY = document.querySelector(".y").value
-        if(drawSizeX > 100 || drawSizeY > 100){
-            return alert("Choose between 1 and 100");
-        }
-        if(drawSizeX < 0 || drawSizeY < 0){
+        if(drawSizeX > 100 || drawSizeY > 100 || drawSizeX < 0 || drawSizeY < 0){
             return alert("Choose between 1 and 100");
         }
         e.preventDefault();
         for(let i = 0; i < drawSizeY; i++){
-            let y = document.createElement('div')
+            let y = document.createElement('div');
             y.className = "pix-row";
-            page.appendChild(y)
+            page.appendChild(y);
 
             for(let j = 0; j < drawSizeX; j++){
                 let x = document.createElement('div')
                 x.className = "pix";
-                y.appendChild(x)
-            }
-        }
+                let r = rgbV();
+                let g = rgbV();
+                let b = rgbV();
+                let a = 0.1;
 
-        const pixels = document.querySelectorAll('.pix');
-        //iterates through nodelist
-        pixels.forEach(px => {
-
-                // add listeners to button as it iterates over them
-                px.addEventListener('mouseenter', () => {
-                    px.style.backgroundColor = "black";
+                x.addEventListener('mouseenter', () =>{
+                    a = Math.min(a + 0.1, 1);
+                    x.style.backgroundColor = `rgba(${r},${g},${b}, ${a})`;
                 });
-            });
-        });
+
+                y.appendChild(x);
+            };
+        };
+    });
 });
+
+function rgbV (){
+    return Math.floor(Math.random() * 256);
+};
